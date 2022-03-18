1551378464f8737828e97ff6818b39c54cf42eeb
Siuol D <nguyencaotan03072000@gmail.com>
2022-03-18 10:48:35 +0200

    Add Ex28

--- PythonEx/Ex28.py
+++ PythonEx/Ex28.py
@@ -1,13 +1,13 @@
 a=0
 b=1
-x=int(input('Input the position number of Fibonacci list:'))
+x=int(input('Input the position numbers of Fibonacci number:'))
 for i in range (2):
     c=a+b
     b=c-a
     a=c
 if c==2:
-    for i in range(x-4):
+    for i in range(x):
         c=a+b
         b=a
         a=c
-print(c)
+        print(c,end=',')

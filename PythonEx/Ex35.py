8991df1e447d27a0e23c32855192b1c822da6843
Siuol D <nguyencaotan03072000@gmail.com>
2022-03-19 23:00:42 +0200

    Add Ex25

--- PythonEx/Ex35.py
+++ PythonEx/Ex35.py
@@ -1,8 +1,8 @@
-x=[3,2,55,4,15,26,76,48,9,10]
-solonnhat=0
+x=[-3,-2,55,-4,1/5,-2/6,-76,-48,-9,10]
+greatest_num=x[1]
 for i in x:
-    if solonnhat-i<0:
-        solonnhat=i
+    if greatest_num<i:
+        greatest_num=i
     else:
         continue
-print('So lon nhat la:',solonnhat)
+print('So lon nhat la:',greatest_num)

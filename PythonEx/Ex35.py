16a18c3590fbeb144bae04376a36b7e0a53dc277
CaoTan <101058559+Siuol7@users.noreply.github.com>
2022-03-20 00:41:46 +0200

    Delete Ex35.py

--- PythonEx/Ex35.py
+++
@@ -1,8 +0,0 @@
-x=[-3,-2,55,-4,1/5,-2/6,-76,-48,-9,10]
-greatest_num=x[1]
-for i in x:
-    if greatest_num<i:
-        greatest_num=i
-    else:
-        continue
-print('So lon nhat la:',greatest_num)

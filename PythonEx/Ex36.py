af46f706b6b934c2b9cc2317aa883966c6e33e93
Siuol D <nguyencaotan03072000@gmail.com>
2022-03-19 23:02:42 +0200

    Add Ex36

--- PythonEx/Ex36.py
+++ PythonEx/Ex36.py
@@ -1,15 +1,14 @@
-x=[3,2,55,4,15,26,76,48,9,10]
-print('In this array')
-greatest_num=0
-sec=0
+x=[-3,-2,55,-4,1/5,-2/6,7/6,-48,-9,10]
+greatest_num=x[1]
+sec=x[1]
 for i in x:
-    if greatest_num-i<0:
+    if greatest_num<i:
         greatest_num=i
     else:
         continue
-print('The greatest number is:',greatest_num)
+print('So lon nhat la:',greatest_num)
 for i in x:
-    if sec-i<0 and i!=greatest_num:
+    if sec<i and i!=greatest_num:
         sec=i
     else:
         continue

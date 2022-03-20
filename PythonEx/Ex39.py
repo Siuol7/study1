2c00c8ec4925fcbcd79183d5b4a62dfd693d2343
Siuol D <nguyencaotan03072000@gmail.com>
2022-03-21 00:58:07 +0200

    Add Ex39

---
+++ PythonEx/Ex39.py
@@ -0,0 +1,12 @@
+x=int(input('Input a number:'))
+y=int(input('Input a number:'))
+def calculation(a,b):
+    c=a+b
+    if a>b:
+        d=a-b
+    else:
+        d=b-a
+    print(c)
+    print(d)
+calculation(x,y)
+    

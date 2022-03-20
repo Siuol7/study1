9fc611b68e6da76680aba66e8a74860d674e3bbc
Siuol D <nguyencaotan03072000@gmail.com>
2022-03-20 03:25:45 +0200

    Add Ex35

---
+++ Ex35.py
@@ -0,0 +1,13 @@
+x = [1,111,2,2,2,2,3,3,3,3,4,4,4,4]
+if x==[]:
+    print('Lack of data')
+else:
+    greatest_num=x[0]
+    sec=x[0]
+    for i in x:
+        if greatest_num<i:
+            greatest_num=i
+        else:
+            continue
+    print('The greatest number is:',greatest_num)
+    

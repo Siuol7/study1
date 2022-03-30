d451df3f8415658651f9ff79ef13ec2cb95d0867
Siuol D <nguyencaotan03072000@gmail.com>
2022-03-31 00:21:56 +0300

    OOP5

---
+++ LearningOOP/OOPEx5.py
@@ -0,0 +1,17 @@
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.capacity * 100
+
+class Bus(Vehicle):
+    def __init__(self,name,mileage,capacity):
+        super().__init__(name,mileage,capacity)
+    def fare(self):
+        return self.capacity*100+10*self.capacity
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
